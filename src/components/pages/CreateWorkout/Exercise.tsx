004065bcc8b23c5716eeebe98c3a11ebdcca827d
Erik <erik.ondra@yahoo.com>
2023-04-29 15:43:51 +0200

    Basic layout for creating workouts

--- src/components/pages/CreateWorkout/Exercise.tsx
+++ src/components/pages/CreateWorkout/Exercise.tsx
@@ -3,26 +3,61 @@ import { Container, Row, Col, Form } from "react-bootstrap";
 
 const Exercise = () => {
   return (
-    <Container className="exercise-div mt-5">
-      <Row>ExerciseName</Row>
-      <Row className="mt-4">
-        <Col className="text-left p-0 sets-col">Sets:</Col>
-        <Col className="p-0">
+    <Container>
+      <div className="exercise-div mt-5">
+        <Row>
           <Form.Control
             type="text"
-            placeholder="Enter sets"
-            className="sets-input"
+            placeholder="Exercise Name"
+            className="w-placeholder ex-name"
           />
-        </Col>
-        <Col className="text-left p-0 focus-col">Focus:</Col>
-        <Col className="p-0">
-          <Form.Control
-            as="textarea"
-            rows={2}
-            placeholder="Focused Muscle groups"
-          />
-        </Col>
-      </Row>
+        </Row>
+        <Row className="pt-4 align-items-center setsAndFocus">
+          <span className="text-left p-0 sets-col">Sets:</span>
+          <Col>
+            <Form.Control
+              type="text"
+              placeholder="Sets"
+              className=" w-placeholder"
+            />
+          </Col>
+          <span className="text-left p-0 sets-col">Focus:</span>
+          <Col>
+            <Form.Control
+              as="textarea"
+              rows={1}
+              placeholder="Focused Muscle groups"
+              className="w-placeholder focus-area pl-0"
+              readOnly
+            />
+          </Col>
+        </Row>
+      </div>
+      <div className="set-div">
+        <Row>
+          <Row>Set 1</Row>
+          <Row className="align-items-center mt-3">
+            {" "}
+            <span className="text-left p-0 sets-col">Reps:</span>
+            <Col>
+              <Form.Control
+                type="text"
+                placeholder="Reps"
+                className=" w-placeholder"
+              />
+            </Col>
+            <span className="text-left p-0 sets-col">Weight:</span>
+            <Col className="weight-input-col">
+              <Form.Control
+                type="text"
+                placeholder="Weight"
+                className="w-placeholder "
+              />
+            </Col>
+            <span>kg</span>
+          </Row>
+        </Row>
+      </div>
     </Container>
   );
 };
