ac657850c5b05e744d8633179ba7fcd37abdd7ce
Erik <erik.ondra@yahoo.com>
2023-05-09 18:49:23 +0200

    small changes

--- src/components/pages/CreateWorkout/ReconstructWorkout.tsx
+++ src/components/pages/CreateWorkout/ReconstructWorkout.tsx
@@ -12,6 +12,7 @@ import {
   setWorkout,
 } from "../../../redux/reducers/workoutSlice";
 import { removeExerciseName } from "../../../redux/reducers/exerciseListSlice";
+import { v4 as uuidv4 } from "uuid";
 
 //todo: make edit sets, add set and remove sets possible
 
